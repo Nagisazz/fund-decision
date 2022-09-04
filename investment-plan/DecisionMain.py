5493b28e3799d9150e7b149ecd022b3077c25bfb
NagisaZZ <597306107@qq.com>
2022-09-04 22:45:45 +0800

    定投主入口方法

--- investment-plan/DecisionMain.py
+++ investment-plan/DecisionMain.py
@@ -1,42 +1,41 @@
 import pandas as pd
+import argparse
 from FundDataCrawler import FundDataCrawler
 from FundDecision import FundDecision
 
 fundDataCrawler = FundDataCrawler()
 fundDecision = FundDecision()
-total = 30000
 frequence = 7
 wave = 5
 days = 250
 balance = 10
 
-for code in ['161725']:
-    fundData = fundDataCrawler.getFund(code)
-    data = pd.read_csv('./investment-plan/train/' + code +
-                       '.csv', header=None, dtype=object)
-    for cell in data.values.tolist():
-        start_time = pd.to_datetime(cell[0])
-        end_time = pd.to_datetime(cell[1])
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--code", help="fund code")
+parser.add_argument("-d", "--date", help="date")
+parser.add_argument("-f", "--frequence", help="frequence")
+parser.add_argument("-i", "--invest", help="invest")
+parser.add_argument("-b", "--balance", help="balance")
+parser.add_argument("-w", "--worth", help="worth")
+parser.add_argument("-y", "--yields", help="yields")
+parser.add_argument("-m", "--mean", help="mean")
+parser.add_argument("-v", "--wave", help="wave")
+parser.add_argument("-t", "--type", help="type")
 
-        df = fundDecision.getData(fundData, start_time, end_time)
-        start_time = fundDecision.getStartTime(df, start_time)
+# 解析参数
+args = parser.parse_args()
+code = args.code
+date = args.date
+frequence = int(args.frequence)
+invest_money = int(args.invest)
+balance = int(args.balance)
+current_worth = float(args.worth)
+current_yield = float(args.yields)
+mean_days = int(args.mean)
+wave_days = int(args.wave)
+type = int(args.type)
 
-        realDays = (pd.to_datetime(end_time) - start_time).days
-        invest_money = total / (realDays / frequence) * 2  # 每次定投金额
-        print("{}开始计算，定投频率：{}日，每次定投：{}，定投开始时间：{}，定投结束时间：{}".format(code, frequence, invest_money, start_time, end_time))
-
-        # res, buy, money = fundDecision.smart_invest(fundData, df, frequence, invest_money, start_time, end_time, days, wave, 1, 3000)
-        # print(money)
-        # fundDecision.myplot(df, res, buy, "定投频率：{}日，每次定投：{}，收益率：".format(frequence, invest_money))
-
-        # res, buy, money = fundDecision.smart_invest(fundData, df, frequence, invest_money, start_time, end_time, days, wave, 2, 3000)
-        # print(money)
-        # fundDecision.myplot(df, res, buy, "定投频率：{}日，每次定投：{}，收益率：".format(frequence, invest_money))
-
-        # res, buy, money = fundDecision.smart_invest(fundData, df, frequence, invest_money, start_time, end_time, days, wave, 3, 3000)
-        # fundDecision.myplot(df, res, buy, "定投频率：{}日，每次定投：{}，收益率：".format(frequence,invest_money))
-
-        fundDecision.calculate_invest(fundData, '20190821', 7, invest_money, 7813, 0.9738, 0.053, days, wave, 1)
-
-        fundDecision.calculate_invest(fundData, '20190828', 7, invest_money, 12492, 0.9915, 0.055, days, wave, 1)
-
+fundData = fundDataCrawler.getFund(code)
+all, rate, price, mean, wave, last_yield = fundDecision.calculate_invest(fundData, date, frequence,
+ invest_money, balance, current_worth, current_yield, mean_days, wave_days, type)
+print(all, rate, price, mean, wave, last_yield)
